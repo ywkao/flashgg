a3e3197ca257207f088abc18302babe3611a04f1
Samuel May <sammay@ucla.edu>
2019-03-22 15:31:01 -0700

    First working versions of custom microAOD production and Tagger + Dumper scripts

---
+++ MicroAOD/test/microAODstd_custom.py
@@ -0,0 +1,135 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.Utilities.FileUtils as FileUtils
+
+from flashgg.MetaData.MetaConditionsReader import *
+
+process = cms.Process("FLASHggMicroAOD")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff") # gives deprecated message in 80X but still runs
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( 1000 ) )
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 100 )
+
+import os
+### 2016
+process.GlobalTag = GlobalTag(process.GlobalTag, '', '')
+process.source = cms.Source("PoolSource",
+			     fileNames=cms.untracked.vstring("/store/data/Run2016B/DoubleEG/MINIAOD/17Jul2018_ver2-v1/00000/12864328-3D8E-E811-B0CE-008CFA11123C.root"))
+                             #fileNames=cms.untracked.vstring("/store/mc/RunIISummer16MiniAODv3/VBFHToGG_M125_13TeV_amcatnlo_pythia8_v2/MINIAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v1/50000/38128C3C-892D-E911-AC8E-008CFA0087C4.root"))
+#    process.GlobalTag = GlobalTag(process.GlobalTag,'80X_dataRun2_2016LegacyRepro_v4','')
+#    process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/data/Run2016B/SingleElectron/MINIAOD/07Aug17_ver1-v1/110000/0248293E-578B-E711-A639-44A842CFC9D9.root"))
+
+### 2017
+# process.GlobalTag = GlobalTag(process.GlobalTag,'','')
+# process.source = cms.Source("PoolSource",
+#                             fileNames=cms.untracked.vstring("/store/mc/RunIIFall17MiniAODv2/GluGluHToGG_M-125_13TeV_powheg_pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/40000/0866D1A8-1941-E811-B61F-0CC47AF9B2E6.root"))
+#    process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring('/store/mc/RunIISummer16MiniAODv2/GluGluHToGG_M-125_13TeV_powheg_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/60000/024E4FA3-8BBC-E611-8E3D-00266CFFBE88.root'))
+    #process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring('root://eoscms.cern.ch//eos/cms/store/mc/RunIIFall17MiniAOD/GJet_Pt-20to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/40000/4A2ACB0A-1BD9-E711-AF54-141877410316.root'))
+#    process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring('root://eoscms.cern.ch//eos/cms/store/mc/RunIIFall17MiniAOD/GluGluToHHTo2B2G_node_SM_13TeV-madgraph/MINIAODSIM/94X_mc2017_realistic_v10-v1/00000/2E0E165D-8E05-E811-909C-FA163E80AE1F.root'))
+#    process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring('file:/afs/cern.ch/user/s/sethzenz/work/public/GluGluHToGG_M125_13TeV_amcatnloFXFX_pythia8_94X_mc2017_realistic_v10-v1.root'))
+#    process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/mc/RunIIFall17MiniAODv2/THQ_ctcvcp_HToGG_M125_13TeV-madgraph-pythia8_TuneCP5/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/90000/6E58A5DD-BF43-E811-8946-0023AEEEB538.root"))
+
+### 2018
+     # process.GlobalTag = GlobalTag(process.GlobalTag,'100X_upgrade2018_realistic_v10','')
+     # process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/data/Run2018B/DoubleMuon/MINIAOD/PromptReco-v1/000/317/080/00000/4E78B565-8464-E811-BF54-02163E01A0FC.root"))
+    #process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/data/Run2018D/EGamma/MINIAOD/PromptReco-v2/000/322/106/00000/9A1C4C91-1EB3-E811-A238-02163E0150CE.root"))
+    #process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/data/Run2018D/DoubleMuon/MINIAOD/PromptReco-v2/000/320/673/00000/0A83E8DF-EB97-E811-AE18-FA163E192E5D.root"))
+    # process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/mc/RunIISpring18MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/100X_upgrade2018_realistic_v10-v2/10000/F24C5C06-FF47-E811-9C2F-FA163EC3CAD1.root"))
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService")
+process.RandomNumberGeneratorService.flashggRandomizedPhotons = cms.PSet(
+          initialSeed = cms.untracked.uint32(16253245)
+        )
+
+#process.GlobalTag = GlobalTag(process.GlobalTag,'92X_upgrade2017_realistic_v10','')
+#process.source.fileNames=cms.untracked.vstring("/store/mc/RunIISummer17MiniAOD/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/92X_upgrade2017_realistic_v10_ext1-v2/10000/00F9D855-E293-E711-B625-02163E014200.root")
+
+# 2017 Data
+#process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/data/Run2017A/DoubleEG/MINIAOD/PromptReco-v2/000/296/173/00000/C24ABCFB-644C-E711-8A5E-02163E01A21C.root"))
+
+# Legacy ReReco
+#process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/data/Run2016B/SingleElectron/MINIAOD/18Apr2017_ver1-v1/120000/40167FB6-6237-E711-934A-001E67E69E05.root"))
+
+#Moriond17 MC
+#process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/mc/RunIISummer16MiniAODv2/GluGluHToGG_M-125_13TeV_powheg_pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/60000/024E4FA3-8BBC-E611-8E3D-00266CFFBE88.root"))
+
+#80x reminiAOD data
+#process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/data/Run2016G/DoubleEG/MINIAOD/03Feb2017-v1/100000/002F14FF-D0EA-E611-952E-008CFA197AF4.root"))
+
+#80x signal
+#process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/mc/RunIISpring16MiniAODv2/GluGluHToGG_M125_13TeV_amcatnloFXFX_pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext2-v1/10000/6A31A211-063B-E611-98EC-001E67F8F727.root")) # ggH 125 miniAODv2
+#process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/mc/RunIISpring16MiniAODv2/ttHJetToGG_M125_13TeV_amcatnloFXFX_madspin_pythia8_v2/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/80000/267A1DB4-3D3B-E611-9AD2-003048C559C4.root")) # ttH 125 miniAODv2
+
+#80x data
+#process.source = cms.Source("PoolSource",fileNames=cms.untracked.vstring("/store/data/Run2016B/DoubleEG/MINIAOD/PromptReco-v2/000/273/158/00000/1E5ABF54-E019-E611-AAED-02163E01293F.root")) # /DoubleEG/Run2016B-PromptReco-v2/MINIAOD
+
+process.MessageLogger.cerr.threshold = 'ERROR' # can't get suppressWarning to work: disable all warnings for now
+# process.MessageLogger.suppressWarning.extend(['SimpleMemoryCheck','MemoryCheck']) # this would have been better...
+
+# Uncomment the following if you notice you have a memory leak
+# This is a lightweight tool to digg further
+#process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
+#                                        ignoreTotal = cms.untracked.int32(1),
+#                                        monitorPssAndPrivate = cms.untracked.bool(True)
+#                                       )
+
+process.load("flashgg/MicroAOD/flashggMicroAODSequence_cff")
+
+# NEEDED FOR ANYTHING PRIOR TO reMiniAOD
+#process.weightsCount.pileupInfo = "addPileupInfo"
+
+from flashgg.MicroAOD.flashggMicroAODOutputCommands_cff import microAODDefaultOutputCommand
+process.out = cms.OutputModule("PoolOutputModule", fileName = cms.untracked.string('myMicroAODOutputFile.root'),
+                               outputCommands = microAODDefaultOutputCommand
+                               )
+
+# All jets are now handled in MicroAODCustomize.py
+# Switch from PFCHS to PUPPI with puppi=1 argument (both if puppi=2)
+
+process.load('RecoMET.METFilters.BadChargedCandidateFilter_cfi')
+process.load('RecoMET.METFilters.globalTightHalo2016Filter_cfi')
+process.load('RecoMET.METFilters.BadPFMuonFilter_cfi')
+
+process.BadChargedCandidateFilter.muons = cms.InputTag("slimmedMuons")
+process.BadChargedCandidateFilter.PFCandidates = cms.InputTag("packedPFCandidates")
+process.BadPFMuonFilter.muons = cms.InputTag("slimmedMuons")
+process.BadPFMuonFilter.PFCandidates = cms.InputTag("packedPFCandidates")
+
+process.flag_globalTightHalo2016Filter = cms.Path(process.globalTightHalo2016Filter)
+process.flag_BadChargedCandidateFilter = cms.Path(process.BadChargedCandidateFilter)
+process.flag_BadPFMuonFilter = cms.Path(process.BadPFMuonFilter)
+
+process.p = cms.Path(process.flashggMicroAODSequence)
+process.e = cms.EndPath(process.out)
+
+# Uncomment these lines to run the example commissioning module and send its output to root
+#process.commissioning = cms.EDAnalyzer('flashggCommissioning',
+#                                       PhotonTag=cms.untracked.InputTag('flashggPhotons'),
+#                                       DiPhotonTag = cms.untracked.InputTag('flashggDiPhotons'),
+#                                       VertexTag=cms.untracked.InputTag('offlineSlimmedPrimaryVertices')
+#)
+#process.TFileService = cms.Service("TFileService",
+#                                   fileName = cms.string("commissioningTree.root")
+#)
+#process.p *= process.commissioning
+
+from flashgg.MicroAOD.MicroAODCustomize import customize
+customize(process)
+
+if "DY" in customize.datasetName or "SingleElectron" in customize.datasetName or "DoubleEG" in customize.datasetName or "EGamma" in customize.datasetName:
+    customize.customizeHLT(process)
+
+### Global Tag
+from Configuration.AlCa.GlobalTag import GlobalTag
+#if customize.processId == "Data":
+#    process.GlobalTag.globaltag = str(customize.metaConditions['globalTags']['data'])
+#else:
+#    process.GlobalTag.globaltag = str(customize.metaConditions['globalTags']['MC'])
+
+#open('dump.py', 'w').write(process.dumpPython())
---
+++ Taggers/test/ttH_TagAndDump.py
@@ -0,0 +1,221 @@
+import sys, os
+
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument("--file_names", help = "csv list of input filenames", type=str)
+#parser.add_argument("--conditionsJSON", help = "MetaConditions JSON file", type=str)
+#parser.add_argument("--max_events", help = "number of events to loop over", type=int, default = -1)
+#args = parser.parse_args()
+
+for arg in sys.argv:
+  print arg
+
+n_events = -1 
+file_names = "DoubleEG"
+#file_names = sys.argv[1].replace("/hadoop", "file:/hadoop").split(",")
+#meta_conditions = sys.argv[2]
+
+import FWCore.ParameterSet.Config as cms
+import FWCore.Utilities.FileUtils as FileUtils
+import FWCore.ParameterSet.VarParsing as VarParsing
+from flashgg.Systematics.SystematicDumperDefaultVariables import minimalVariables,minimalHistograms,minimalNonSignalVariables,systematicVariables
+from flashgg.Systematics.SystematicDumperDefaultVariables import minimalVariablesHTXS,systematicVariablesHTXS
+import os
+from flashgg.MetaData.MetaConditionsReader import *
+
+# SYSTEMATICS SECTION
+dropVBFInNonGold = False  # for 2015 only!
+
+process = cms.Process("FLASHggTag")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1000 )
+
+
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring("file:/hadoop/cms/store/user/smay/ttH/MicroAOD/RunII/DoubleEG_Run2016B-17Jul2018_ver2-v1_MINIAOD_RunII/test_skim_1.root"))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(n_events))
+
+
+systlabels = [""]
+phosystlabels = []
+metsystlabels = []
+jetsystlabels = []
+elesystlabels = []
+musystlabels = []
+
+from flashgg.MetaData.MetaConditionsReader import *
+
+# set default options if needed
+metaConditions = MetaConditionsReader("MetaData/data/MetaConditions/Era2016_RR-17Jul2018_v1.json")
+
+ISDATA = False
+if "DoubleEG" in file_names[0] or "EGamma" in file_names[0]:
+  ISDATA = True
+
+ISSIGNAL = False
+if "ttH" in file_names[0]:
+  ISSIGNAL = True
+
+
+### Global Tag
+from Configuration.AlCa.GlobalTag import GlobalTag
+if ISDATA:
+    process.GlobalTag.globaltag = str(metaConditions['globalTags']['data'])
+else:
+    process.GlobalTag.globaltag = str(metaConditions['globalTags']['MC'])
+
+from flashgg.Systematics.SystematicsCustomize import *
+class customizer:
+  def __init__(self, **kwargs):
+    self.metaConditions = kwargs.get("metaConditions")
+    self.processType = kwargs.get("processType")
+
+customize = customizer(metaConditions = metaConditions, processType = "data" if ISDATA else "mc")
+jetSystematicsInputTags = createStandardSystematicsProducers(process , customize)
+modifyTagSequenceForSystematics(process,jetSystematicsInputTags)
+
+# needed for 0th vertex from microAOD
+process.load("flashgg/MicroAOD/flashggDiPhotons_cfi")
+process.flashggDiPhotons.whichVertex = cms.uint32(0)
+process.flashggDiPhotons.useZerothVertexFromMicro = cms.bool(True)
+
+# Remove unneeded tags
+process.flashggTagSequence.remove(process.flashggTTHDiLeptonTag)
+process.flashggTagSequence.remove(process.flashggVBFTag)
+process.flashggTagSequence.remove(process.flashggVHMetTag)
+process.flashggTagSequence.remove(process.flashggWHLeptonicTag)
+process.flashggTagSequence.remove(process.flashggZHLeptonicTag)
+process.flashggTagSequence.remove(process.flashggVHLeptonicLooseTag)
+process.flashggTagSequence.remove(process.flashggVHHadronicTag)
+process.flashggTagSequence.remove(process.flashggUntagged)
+process.flashggTagSequence.remove(process.flashggVBFMVA)
+process.flashggTagSequence.remove(process.flashggVBFDiPhoDiJetMVA)
+
+process.flashggTagSorter.TagPriorityRanges = cms.VPSet(   
+        cms.PSet(TagName = cms.InputTag('flashggTTHLeptonicTag')),
+        cms.PSet(TagName = cms.InputTag('flashggTTHHadronicTag')) )
+
+newvpset = cms.VPSet()
+for pset in process.flashggDiPhotonSystematics.SystMethods:
+    if not pset.Label.value().count("FracRVNvtxWeight") :
+	print  pset.Label.value()
+	newvpset += [pset]
+#from flashgg.Systematics.flashggDiPhotonSystematics_cfi import PixelSeedWeight
+#newvpset += [ PixelSeedWeight ]
+
+process.flashggDiPhotonSystematics.SystMethods = newvpset
+
+
+# Or use the official tool instead
+useEGMTools(process)
+
+if ISSIGNAL:
+  print "Signal MC, so adding systematics and dZ"
+  customizeSystematicsForSignal(process)
+elif ISDATA:
+  print "Data, so turn off all shifts and systematics, with some exceptions"
+  variablesToUse = minimalNonSignalVariables
+  customizeSystematicsForData(process)
+else:
+  print "Background MC, so store mgg and central only"
+  variablesToUse = minimalNonSignalVariables
+  customizeSystematicsForBackground(process)
+
+printSystematicInfo(process)
+
+from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
+hlt_paths = []
+for dset in metaConditions["TriggerPaths"]:
+  hlt_paths.extend(metaConditions["TriggerPaths"][dset])
+for i in range(len(hlt_paths)):
+  hlt_paths[i] = hlt_paths[i].encode("ascii")
+process.hltHighLevel= hltHighLevel.clone(HLTPaths = cms.vstring(hlt_paths))
+
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+# ee bad supercluster filter on data
+process.load('RecoMET.METFilters.eeBadScFilter_cfi')
+process.eeBadScFilter.EERecHitSource = cms.InputTag("reducedEgamma","reducedEERecHits") # Saved MicroAOD Collection (data only)
+process.dataRequirements = cms.Sequence()
+if ISDATA: 
+        process.dataRequirements += process.hltHighLevel
+        process.dataRequirements += process.eeBadScFilter
+
+
+process.genFilter = cms.Sequence()
+
+process.load("flashgg/Taggers/flashggTagSequence_cfi")
+process.load("flashgg/Taggers/flashggTagTester_cfi")
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("merged_ntuple.root"))
+## TAGS DUMPERS ##
+from flashgg.Taggers.tagsDumpers_cfi import *
+
+process.tthLeptonicTagDumper = createTagDumper("TTHLeptonicTag")
+process.tthHadronicTagDumper = createTagDumper("TTHHadronicTag")
+
+process.tthLeptonicTagDumper.dumpTrees = True
+process.tthHadronicTagDumper.dumpTrees = True
+
+## define categories and associated objects to dump
+import flashgg.Taggers.dumperConfigTools as cfgTools
+
+dipho_variables=["dipho_sumpt      := diPhoton.sumPt",
+                 "dipho_cosphi     := abs(cos(diPhoton.leadingPhoton.phi - diPhoton.subLeadingPhoton.phi))",
+                 "mass             := diPhoton.mass",
+                 "leadPt           := diPhoton.leadingPhoton.pt",
+                 "leadEt           := diPhoton.leadingPhoton.et",
+		 "leadEnergy       := diPhoton.leadingPhoton.energy",
+                 "leadEta          := diPhoton.leadingPhoton.eta",
+                 "leadPhi          := diPhoton.leadingPhoton.phi",
+                 "lead_sieie       := diPhoton.leadingPhoton.sigmaIetaIeta",
+                 "lead_hoe         := diPhoton.leadingPhoton.hadronicOverEm",
+                 "lead_sigmaEoE    := diPhoton.leadingPhoton.sigEOverE",
+                 "lead_ptoM        := diPhoton.leadingPhoton.pt/diPhoton.mass",
+                 "leadR9           := diPhoton.leadingPhoton.full5x5_r9"]
+
+## TTH leptonic ##
+cfgTools.addCategories(process.tthLeptonicTagDumper,
+                       ## categories definition
+                       [("all","1",0)
+                    ],
+                       ## variables to be dumped in trees/datasets. Same variables for all categories
+                       variables=dipho_variables+
+                       ["n_ele    := electrons.size"
+		],
+                       ## histograms
+                       histograms=[]
+)
+
+## TTH hadronic ##
+cfgTools.addCategories(process.tthHadronicTagDumper,
+                       ## categories definition
+                       [("all","1",0)
+                    ],
+                       ## variables to be dumped in trees/datasets. Same variables for all categories
+                       variables=dipho_variables+
+                       ["n_bjets  := nBMedium",
+                        "n_jets   := jetVector.size"
+		],
+                       ## histograms
+                       histograms=[]
+)
+
+process.p = cms.Path(process.dataRequirements*
+                         #process.genFilter*
+                         process.flashggDiPhotons* # needed for 0th vertex from microAOD
+                         process.flashggUpdatedIdMVADiPhotons*
+                         #process.flashggDiPhotonSystematics*
+                         process.flashggMetSystematics*
+                         process.flashggMuonSystematics*process.flashggElectronSystematics*
+                         (process.flashggUnpackedJets*process.jetSystematicsSequence)*
+                         (process.flashggTagSequence*process.systematicsTagSequences)*
+			 process.flashggTagSequence*
+                         process.flashggTagTester*
+		         (process.tthLeptonicTagDumper
+                          +process.tthHadronicTagDumper))
