6b0f63dc41dc1e785ade339d0c2327a066b45c52
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2020-10-07 05:43:09 +0200

    Minor modified

--- Systematics/test/workspaceStd.py
+++ Systematics/test/workspaceStd.py
@@ -132,7 +132,6 @@ customize.options.register('doMuFilter',
                            VarParsing.VarParsing.varType.bool,
                            'doMuFilter'
                            )
-                           )
 customize.options.register('acceptance',
                            'NONE',
                            VarParsing.VarParsing.multiplicity.singleton,
@@ -460,7 +459,7 @@ if customize.processId == "tHq":
 
 tag_only_variables = {}
 
-elif customize.tthTagsOnly:
+if customize.tthTagsOnly:
     tagList=[
         ["TTHHadronicTag",4],
         ["TTHLeptonicTag",4]
