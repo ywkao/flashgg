b2fbb4d11567eceafdb70a6569b4a6df7dc19bfa
Samuel May <sammay@ucla.edu>
2021-01-11 16:36:19 -0800

    Save central weight for c-tag reshape SF

--- Taggers/test/ttH_TagAndDump.py
+++ Taggers/test/ttH_TagAndDump.py
@@ -210,14 +210,13 @@ if not ISDATA:
             variablesToUse.append("ElectronWeight%s01sigma[1,-999999.,999999.] := getObjectWeight(\"ElectronWeight%s01sigma\")" % (direction,direction))
             #variablesToUse.append("JetBTagCutWeight%s01sigma[1,-999999.,999999.] := getObjectWeight(\"JetBTagCutWeight%s01sigma\")" % (direction,direction))
             variablesToUse.append("JetBTagReshapeWeight%s01sigma[1,-999999.,999999.] := getObjectWeight(\"JetBTagReshapeWeight%s01sigma\")" % (direction,direction))
-            #variablesToUse.append("JetCTagReshapeWeight%s01sigma[1,-999999.,999999.] := getObjectWeight(\"JetCTagReshapeWeight%s01sigma\")" % (direction,direction))
             for sourceName in customize.metaConditions['cTagSystematics']['listOfSources']:
                 variablesToUse.append("JetCTagReshapeWeight%s%s01sigma[1,-999999.,999999.] := weight(\"JetCTagReshapeWeight%s%s01sigma\")" % (str(sourceName),direction,str(sourceName),direction))
             variablesToUse.append("genTopPtReweight%s01sigma[1,-999999.,999999.] := weight(\"genTopPtReweight%s01sigma\")" % (direction,direction))
             if applyL1Prefiring:
                 variablesToUse.append("prefireWeight%s01sigma[1,-999999.,999999.] := weight(\"prefireWeight%s01sigma\")" % (direction,direction))
             variablesToUse.append("weight_JetBTagWeight:=getObjectWeight(\"JetBTagReshapeWeightCentral\")")
-            #variablesToUse.append("weight_JetCTagWeight:=getObjectWeight(\"JetCTagReshapeWeightCentral\")")
+            variablesToUse.append("weight_JetCTagWeight:=getObjectWeight(\"JetCTagReshapeWeightCentral\")")
             for r9 in ["HighR9","LowR9"]:
                 for region in ["EB","EE"]:
                     phosystlabels.append("ShowerShape%s%s%s01sigma"%(r9,region,direction))
